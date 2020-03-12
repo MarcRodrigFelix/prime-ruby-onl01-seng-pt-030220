ac854a376a95b90a5e408e1cd2c4834600cd3d8e
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-12 02:24:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,8 @@
-# Add  code here!
+# Create a range of numbers from 2 to one less than the number in question (num - 1)
+# Loop through that range
+# Ask if the number in question (num) is cleanly divisible by each number in the range (n)
+# If any one of those range numbers (n) creates a 0 remainder when the number in question (num) is divided by that number (n), the number in question (num) is not prime
+# If at the end of the loop, none of the range numbers (n) created a 0 remainder, the number in question (num) is, indeed, prime
 
 def prime?(num)
   num_arr = (2..(num - 1))
@@ -9,5 +13,6 @@ def prime?(num)
    else
      puts "Number is prime!"
    end
+ end
 
 end
