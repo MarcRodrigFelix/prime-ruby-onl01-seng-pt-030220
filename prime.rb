d9ee0b52341efee5879eaa931f13d716b9bdecde
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-12 02:21:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
 # Add  code here!
 
-def prime?()
+def prime?(num)
+  num_arr = (2..(num - 1))
+
+  num_arr.each do |n|
+   if num % n == 0
+     false
+   else
+     puts "Number is prime!"
+   end
+
+
 end
