1322ad2999388c5bca1d024861645d4f4574da7b
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-12 03:26:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,33 +4,37 @@
 # If any one of those range numbers (n) creates a 0 remainder when the number in question (num) is divided by that number (n), the number in question (num) is not prime
 # If at the end of the loop, none of the range numbers (n) created a 0 remainder, the number in question (num) is, indeed, prime
 
+# def prime?(num)
+#   # if num < 0
+#   #   return false
+#   # end
+# 
+#   return false if num < 0
+# 
+#   num_arr = (2..(num - 1)).to_a #your number, turn it range of numbers
+# 
+#   num_arr.none? { |n| num % n == 0 }
+# 
+#     # if num % n == 0
+#     #   return false
+#     # else
+#     #   return true
+#     # end
+#   # end
+# 
+# end
+
 def prime?(num)
-  # if num < 0
-  #   return false
-  # end
-
-  return false if num < 0
-
-  num_arr = (2..(num - 1)).to_a #your number, turn it range of numbers
-
-  num_arr.none? { |n| num % n == 0 }
-
-    # if num % n == 0
-    #   return false
-    # else
-    #   return true
-    # end
-  # end
-
+  if num < 0
+    return false
+  else 
+        
+  n = 2
+  while n < num
+    return false if num % n == 0 || num % n !== 0
+    n += 1
+  end
+  true
 end
 
-# def prime?(num)
-#   if num < 0
-#     return false
-#   n = 2
-#   while n < num
-#     return false if num % n == 0 || num % n !== 0
-#     n += 1
-#   end
-#   true
-# end
+end
