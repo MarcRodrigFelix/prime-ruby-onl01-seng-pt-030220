2994406bd98fb9a8c1330539009ff7f82430413e
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-12 02:54:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -19,14 +19,9 @@
 
 def prime?(num)
   n = 2
-
   while n < num
-    if num % n == 1
-      true
-      break
-    else
-      false
-      n += 1
-    end
-
+    return false if num % n == 0
+    n += 1
+  end
+  true
 end
