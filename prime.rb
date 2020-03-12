fa6c5e51dd5f1cc6c4e6a31a6ba145489864427f
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-12 02:38:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@
 # If at the end of the loop, none of the range numbers (n) created a 0 remainder, the number in question (num) is, indeed, prime
 
 def prime?(num)
-  num_arr = (2..(num - 1)).to_a
+  num_arr = (2..(num - 1)) #your number, turn it range of numbers
 
   num_arr.each do |n|
     return false if num % n == 0
